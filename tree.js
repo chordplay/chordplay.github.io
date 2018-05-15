473995e9368152f6b5d268c38715947d3bf86b24
Lee Ju Been <ljb7977@gmail.com>
2018-05-15 17:25:03 +0900

    selection rough border remove submodule

--- tree.js
+++ tree.js
@@ -22,6 +22,29 @@ $(document).ready(function(){
       }
     }
   })
+
+  $("#invertChordButton").click(function(){
+    var index = score.length;
+    invertChord(index-1, 1);
+    renderScore();
+  })
+
+
+  $("#deleteChordsButton").click(function(){
+    console.log("clicked");
+    var index = score.length;
+    deleteChord(index-1);
+    renderScore();
+  })
+  $("#deleteBarsButton").click(function(){
+    var index = score.length;
+    deleteUnit(index-1);
+    renderScore();
+  })
+  $("#addBarsButton").click(function(){
+    addUnit();
+    renderScore();
+  })
   $(document).on('click', '.deeptree', function(){
 
     var str = $(this).find("p").text();
@@ -31,7 +54,7 @@ $(document).ready(function(){
     addUnit();
     setChord(index, chord);
     renderScore();
-
+    $("#scoreDiv").scrollTop($("#scoreDiv").prop("scrollHeight"));
   })
 
   function openDetails(chordname, trtemp){
