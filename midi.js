4a0308ab46d0748354eebb0abd6237c7b39479bb
Lee Ju Been <ljb7977@gmail.com>
2018-05-17 00:52:44 +0900

    pause function. not perfect

--- midi.js
+++ midi.js
@@ -1,5 +1,9 @@
+var BPM = 78;
+var playing_sources = [];
+var playing_notes = [];
+var play_timer;
+
 window.onload = function () {
-    let BPM = 78;
 
     MIDI.loadPlugin({
         soundfontUrl: "./midi/examples/soundfont/",
@@ -9,31 +13,47 @@ window.onload = function () {
         },
         onsuccess: function() {
             console.log("Loaded");
+            MIDI.setVolume(0, 127);
         }
     });
 
-    document.getElementById("playButton").onclick = function(){
+    $("#playButton").click(function(){
         // console.log($('#bpm').val());
         // console.log(typeof($('#bpm').val()));
 
         // playMIDI(notes, parseInt($('#bpm').val(), 10));
-        //playMIDI(notes, BPM, 10);
         $("#stopButton").show();
         $("#playButton").hide();
-        if(selected_units.length === 0){
-            playMIDI(BPM, score);
+        if(playing_notes.length > 0){
+            ldelay = MIDI.getContext().currentTime;
+            rdelay = MIDI.getContext().currentTime;
+            let notes = playing_notes;
+            playing_notes = [];
+            notes.forEach(note => playNote(note));
+            play_timer = window.setInterval(checkPlayingList, 100);
+            // playMIDI(playing_notes);
         } else {
-            // playMIDI(BPM, selected_units); need to be sorted
+            if (selected_units.length === 0) {
+                playMIDI(score);
+            } else {
+                // playMIDI(selected_units); need to be sorted
+            }
         }
-    };
+    });
 
     $("#stopButton").click(function(){
-        if(selected_units.length === 0){
-            MIDI.stopAll(playing_sources);
-        }
+        MIDI.stopAll(playing_sources);
+        // playing_notes = [];
+        window.clearInterval(play_timer);
+        console.log("pause with: ", playing_notes);
         $("#stopButton").hide();
         $("#playButton").show();
     });
+
+    // $("#pauseButton").click(function(){
+    //     MIDI.stopAll(playing_sources);
+    //     window.clearInterval(play_timer);
+    // })
 };
 
 /*
@@ -42,10 +62,10 @@ pitch: C3, D4...
 type: {4.0, 3.0, 2.0, 1.5, 1.0, 0.75, 0.5, 0.375, 0.25} (온음표 -> 16분음표)
 */
 
-function playNote(note, delay, length){
-    MIDI.noteOn(0, note, 127, delay);
-    MIDI.noteOff(0, note, delay+length);
-}
+// function playNote(note, delay, length){
+//     MIDI.noteOn(0, note, 127, delay);
+//     MIDI.noteOff(0, note, delay+length);
+// }
 
 function playChord(notes, delay, length){
     let source = MIDI.chordOn(0, notes, 127, delay);
@@ -53,48 +73,71 @@ function playChord(notes, delay, length){
     return source;
 }
 
-function playMIDI (bpm, units){
-    let ldelay = MIDI.getContext().currentTime;
-    let rdelay = MIDI.getContext().currentTime;
-    let qLength = 60.0/bpm; //length of quarter note (1 beat)
+function checkPlayingList()
+{
+    let currentTime = MIDI.getContext().currentTime;
 
-    MIDI.setVolume(0, 127);
+    playing_notes = playing_notes.filter(note => note.start>=currentTime);
+    console.log("check: ", playing_notes);
+
+    if(playing_notes.length === 0){
+        window.clearInterval(play_timer);
+        $("#stopButton").hide();
+        $("#playButton").show();
+    }
+}
+
+function playMIDI (units){
+    ldelay = MIDI.getContext().currentTime;
+    rdelay = MIDI.getContext().currentTime;
     playing_sources = [];
+    playing_notes = [];
+
+    console.log(units);
     units.forEach(function(unit){
-
-        function playNotes(note, mode){
-            let type = durationToType(note.duration);
-
-            if(note.is_rest){
-                if(mode === "right")
-                    rdelay += qLength * type;
-                else if (mode === "left")
-                    ldelay += qLength*type;
-            } else {
-                let pitch = note.keys.map(key => {if(key.includes("#")) {
-                    return MIDI.keyToNote[key.replace("#/", "")] + 1;
-                } else {
-                    return MIDI.keyToNote[key.replace("/", "")];
-                }});
-
-                // playNote(MIDI.keyToNote[pitch], delay, qLength * note.type);
-                if(mode === "right"){
-                    let ret = playChord(pitch, rdelay, qLength*type);
-                    playing_sources = playing_sources.concat(ret);
-                    rdelay += qLength * type;
-                } else if (mode === "left"){
-                    let ret = playChord(pitch, ldelay, qLength*type);
-                    playing_sources = playing_sources.concat(ret);
-                    ldelay += qLength * type;
-                } else {
-                    //error
-                }
-            }
-        }
-
-        unit.left.forEach(note => playNotes(note, "left"));
-        unit.right.forEach(note => playNotes(note, "right"));
+        unit.left.forEach(note => playNote(note));
+        unit.right.forEach(note => playNote(note));
     });
+
+    play_timer = window.setInterval(checkPlayingList, 100);
+}
+
+function playNote(note){
+    let qLength = 60.0/BPM; //length of quarter note (1 beat)
+
+    let type = durationToType(note.duration);
+    if(note.is_rest){
+        if(note.hand === "right"){
+            note.start = rdelay;
+            rdelay += qLength * type;
+        }
+        else if (note.hand === "left"){
+            note.start = ldelay;
+            ldelay += qLength*type;
+        }
+    } else {
+        let pitch = note.keys.map(key => {
+            if(key.includes("#")) {
+                return MIDI.keyToNote[key.replace("#/", "")] + 1;
+            } else {
+                return MIDI.keyToNote[key.replace("/", "")];
+            }
+        });
+        if(note.hand === "right"){
+            let ret = playChord(pitch, rdelay, qLength*type);
+            playing_sources = playing_sources.concat(ret);
+            note.start = rdelay;
+            rdelay += qLength * type;
+        } else if (note.hand === "left"){
+            let ret = playChord(pitch, ldelay, qLength*type);
+            playing_sources = playing_sources.concat(ret);
+            note.start = ldelay;
+            ldelay += qLength * type;
+        } else {
+            //error
+        }
+    }
+    playing_notes.push(note);
 }
 
 function durationToType(duration){
